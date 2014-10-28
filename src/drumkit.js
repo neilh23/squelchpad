7874ed8461d46d909934fb631c832199d17c4a5f
Neil Howie <neil.howie@yahoo.co.uk>
2014-10-28 20:03:42 +0000

    working refactored drumkit

--- src/drumkit.js
+++ src/drumkit.js
@@ -1,94 +1,15 @@
-var audioContext,destination,wet,dry,pan;
-
-function bufferSound(event) {
-  var index = event.target.squindex;
-
-
-  audioContext.decodeAudioData(event.target.response, function(buffer) {
-    var bt = $('#bt'+index);
-    bt.on("squelchOn", function(e, args) {
-      var vel = args.velocity;
-
-      wet.gain.value = 1.0 - vel;
-      dry.gain.value = vel;
-
-      if (index == 1) {
-        pan.setPosition(args.xvel, 0, 1-Math.abs(args.xvel));
-      }
-
-      var src = audioContext.createBufferSource();
-      src.buffer = buffer;
-      src.connect(destination[index]);
-      src.noteOn(0);
-    });
-    /*
-    bt.on("squelchOff", function(args) {
-      console.log("Unsquelched " + index);
-    });
-    */
-  });
-}
-
-function startKit() {
-  try {
-    audioContext = new webkitAudioContext();
-  } catch(e) { alert("Web Audio API is not supported in this browser"); }
-
-  $('#bt0').squelch({baseColor: 'red'});
-  $('#bt1').squelch({baseColor: 'green'});
-  $('#bt2').squelch({baseColor: 'blue'});
-  /*
-  $('.squelch').on("squelchOn", function(e, args) {
-    console.log("Squelch: " + args.velocity + "/" + args.xvel + "/" + args.yvel);
-  });
-  */
-
-  var verb = new SimpleReverb(audioContext, { seconds: 1.0, decay: 4, reverse: 0 });
-  var master = audioContext.createGain();
-  wet = audioContext.createGain();
-  dry = audioContext.createGain();
-  pan = audioContext.createPanner();
-  pan.panningModel = 'equalpower';
-
-  pan.connect(verb.input);
-  pan.connect(dry);
-
-  master.connect(verb.input);
-  master.connect(dry);
-
-  verb.connect(wet);
-
-  wet.connect(audioContext.destination);
-  dry.connect(audioContext.destination);
-
-  destination = [ master, pan, master ];
-
-  var samples = [ "sounds/kick.wav", "sounds/hihat-closed.wav", "sounds/hihat-closed.wav", "sounds/snare.wav" ];
-
-  var index = 0;
-  for (var i = 0; i < samples.length; i++) {
-    var request = new XMLHttpRequest();
-    request.open('GET', samples[i], true);
-    request.responseType = 'arraybuffer';
-    request.squindex = i;
-    request.addEventListener('load', bufferSound, false);
-    request.send();
-    if (i != 1) { index++; }
-  }
-}
-
 /*
  * position:
  * +---------------------+
- * |TR        T        TL|
+ * |TL        T        TR|
  * |                     |
  * |                     |
  * |                     |
- * |R         M         L|
+ * |L         M         R|
  * |                     |
  * |                     |
  * |                     |
- * |BR        B        BL|
+ * |BL        B        BR|
  * +---------------------+
  * or no specification for constant
  * e.g.
@@ -113,7 +34,7 @@ function startKit() {
 function DrumPad(args) {
   "use strict";
   this.context = args.context;
-  this.destination = args.destination || context.destination; 
+  this.destination = args.destination || this.context.destination; 
   var el = this.element = args.element;
 
   this.samples = args.samples;
@@ -125,31 +46,36 @@ function DrumPad(args) {
 
   for (var i = 0; i < this.samples.length; i++) {
     var p = this.samples[i];
+
+    p.gainNode = this.context.createGain();
+    p.gainNode.connect(this.destination);
     if (p.pan && !this.hasPan) {
       this.hasPan = true;
 
-      pan = audioContext.createPanner();
-      pan.panningModel = 'equalpower';
-      pan.connect(this.destination);
+      this.pan = this.context.createPanner();
+      this.pan.panningModel = 'equalpower';
+      this.pan.connect(p.gainNode);
     }
     var request = new XMLHttpRequest();
     request.open('GET', p.sample, true);
     request.responseType = 'arraybuffer';
     request.drumpad = this;
-    request.squindex = this;
+    request.squindex = i;
     request.addEventListener('load', function(e) { var t = e.target; t.drumpad.fileLoaded(t.squindex, t.response); }, false);
     request.send();
   }
 }
 
 DrumPad.prototype = Object.create(null, {
+  cornerGain: { value: function(xvel, yvel) {
+    "use strict";
+    if (xvel <= 0) { return 0; }
+    if (yvel <= 0) { return 0; }
+
+    return Math.sqrt(Math.pow(xvel, 2) + Math.pow(yvel, 2));
+  }}, 
   squelch: { value: function(args) {
     "use strict";
-    var vel = args.velocity;
-
-    wet.gain.value = 1.0 - vel;
-    dry.gain.value = vel;
-
     if (this.hasPan) {
       this.pan.setPosition(args.xvel, 0, 1-Math.abs(args.xvel));
     }
@@ -157,31 +83,43 @@ DrumPad.prototype = Object.create(null, {
     for (var i = 0; i < this.samples.length; i++) {
       var p = this.samples[i];
 
-      var src = audioContext.createBufferSource();
-      src.buffer = p.buffer;
-      if (p.pan) {
-        src.connect(this.pan);
-      } else {
-        src.connect(this.destination);
-      }
       var gain = 1.0;
       switch(p.position) {
-        case 'M': gain = vel; break;
-        case 'L': gain = xvel; break;
+        case 'M': gain = args.velocity; break;
+        case 'R': gain = args.xvel; break;
+        case 'L': gain = 1.0 - args.xvel; break;
+        case 'T': gain = args.yvel; console.log("Top: " + gain); break;
+        case 'B': gain = 1.0 - args.yvel; console.log("Bottom: " + gain); break;
+        case 'TR': gain = this.cornerGain(args.xvel, args.yvel); break;
+        case 'TL': gain = this.cornerGain(-args.xvel, args.yvel); break;
+        case 'BR': gain = this.cornerGain(args.xvel, -args.yvel); break;
+        case 'BL': gain = this.cornerGain(-args.xvel, -args.yvel); break;
       }
-      if (p.inverse) {
-        gain = 1.0 - gain;
-      }
-      src.noteOn(0);
+      if (p.inverse) { gain = 1.0 - gain; }
+
+      if (gain <= 0.0) { continue; }
+
+      p.gainNode.gain.value = gain;
+
+      var src = this.context.createBufferSource();
+      var keys = Object.keys(src);
+
+      console.log(keys);
+      src.buffer = p.buffer;
+
+      src.connect(p.gainNode);
+
+      src.start(0);
     }
   }},
   fileLoaded: { value: function(index, response) {
+    var target = this;
     this.context.decodeAudioData(response, function(buffer) {
-      this.samples[index].buffer = buffer;
-      this.loaded++;
-      if (this.loaded >= this.samples.length) {
-        this.element.on("squelchOn", function(e, args) { this.squelch(args); });
-      });
+      target.samples[index].buffer = buffer;
+      target.loaded++;
+      if (target.loaded >= target.samples.length) {
+        target.element.on("squelchOn", function(e, args) { target.squelch(args); });
+      }
     });
   }}
 });
